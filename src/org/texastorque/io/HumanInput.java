d023b33b5d8afd9d7c7afe08fefeec08de6194d7
dlzou <zoudl2000@gmail.com>
2017-12-11 21:51:03 -0600

    implemented arm subsystem

--- src/org/texastorque/io/HumanInput.java
+++ src/org/texastorque/io/HumanInput.java
@@ -21,6 +21,7 @@ public class HumanInput extends Input{
 		DB_leftSpeed = -driver.getLeftYAxis() + driver.getRightXAxis();
 		DB_rightSpeed = -driver.getLeftYAxis() - driver.getRightXAxis();
 	
+		AR_armUp = driver.getAButton();
 	}
 
 	
