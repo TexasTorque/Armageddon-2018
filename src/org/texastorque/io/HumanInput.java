1f71ee44ca1ac82145c5496e3fe40142c5698971
Jonathon Lowe <jonathon.lowe@outlook.com>
2018-01-25 16:31:02 -0600

    removed print statement

--- src/org/texastorque/io/HumanInput.java
+++ src/org/texastorque/io/HumanInput.java
@@ -18,7 +18,6 @@ public class HumanInput extends Input{
 	}
 	
 	public void updateDrive(){
-		System.out.println("Driving...");
 		DB_leftSpeed = -driver.getLeftYAxis() + driver.getRightXAxis();
 		DB_rightSpeed = -driver.getLeftYAxis() - driver.getRightXAxis();
 	
