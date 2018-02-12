58c577fc262babebe8d80b9aedddf4d3913f4cbe
Jonathon Lowe <jonathon.lowe@outlook.com>
2018-02-12 16:36:03 -0600

    changed ports

--- src/org/texastorque/constants/Ports.java
+++ src/org/texastorque/constants/Ports.java
@@ -2,8 +2,14 @@ package org.texastorque.constants;
 
 import edu.wpi.first.wpilibj.DigitalSource;
 
-
 public class Ports {
+	
+	public static final int DB_LEFT_ENCODER_A = 0;
+	public static final int DB_LEFT_ENCODER_B = 1;
+	public static final int DB_RIGHT_ENCODER_A = 2;
+	public static final int DB_RIGHT_ENCODER_B = 3;
+	public static final int AM_ENCODER_A = 4;
+	public static final int AM_ENCODER_B = 5;
 
 	//Drivebase
 	public static final int DB_LEFT_FORE_PORT = 2;
@@ -11,29 +17,23 @@ public class Ports {
 	public static final int DB_RIGHT_FORE_PORT = 0;
 	public static final int DB_RIGHT_REAR_PORT = 1;
 	
-	public static final int DB_LEFT_ENCODER_A = 0;
-	public static final int DB_LEFT_ENCODER_B = 1;
-	public static final int DB_RIGHT_ENCODER_A = 2;
-	public static final int DB_RIGHT_ENCODER_B = 3;
-	
-	
 	//Arm
-	public static final int AM_RIGHT_PORT = 4;
-	public static final int AM_LEFT_PORT = 5;
-	
+	public static final int AM_RIGHT_PORT= 8;
+	public static final int AM_LEFT_PORT= 9;
+
+	public static final int PT_SOLE = 4;	
+
+	public static final int IN_LEFT= 5;
+	public static final int IN_RIGHT = 6;
 	//Claw
-	public static final int CL_PORT_A = 2;
-	public static final int CL_PORT_B = 3;
-	
+	public static final int IN_DOWN_A = 4;
+	public static final int IN_DOWN_B = 5;
+	public static final int IN_OUT_A = 2;
+	public static final int IN_OUT_B = 3;
 
-	public static final int PT_SOLE = 8;
+	public static final int CL_PORT_A = 0;
+	public static final int CL_PORT_B = 1;
 
-	public static final int IN_LEFT= 9;
-	public static final int IN_RIGHT = 10;
-	public static final int IN_DOWN_A = 6;
-	public static final int IN_DOWN_B = 7;
-	public static final int IN_OUT_A = 4;
-	public static final int IN_OUT_B = 5;
 
 
 }
